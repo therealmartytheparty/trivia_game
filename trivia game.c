060c3505e9f913ebf1116cdb96546b90b630aec4
therealmartytheparty <45380343+therealmartytheparty@users.noreply.github.com>
2018-11-27 01:19:51 -0500

    Add files via upload

---
+++ trivia game.c
@@ -0,0 +1,38 @@
+//Trivia_game
+//prints a question and then gives 3-4 options for answers.
+//options are outlined as a,b,c,d
+
+//The code would first display the question
+//then the code would display the 3-4 options for that question
+//the can pick a answer by pressing a,b,c,d on the keyboard\
+//the values
+// a = 97
+// b = 98
+// c = 99
+// d = 100
+
+
+//code for the first question
+
+//code for the first set of answers
+
+#include <stdio.h>
+#include <string.h>
+
+void drawcharacter(int,int,char);
+void drawpixel(int,int,short);
+void delaytime(volatile int);
+void writeText(int,int,char *);
+void clearScreenWithCharacters();
+
+int main()
+{
+
+  char buffer[20],buffer2[20];
+  int n1 =100 , n2 = 300;
+  int x1_start, y1_start;
+  int x2_start, y2_start;
+  int time, i;
+
+  st
+}
